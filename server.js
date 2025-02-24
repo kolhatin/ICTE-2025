337dfb6b3095016b885791cba7089e80b3bc8364
kolhatin <kolhatin.a@gmail.com>
2025-02-24 19:45:01 +0200

    moved to local https

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const http = require('http');
+
+const https = require('node:https');
+const fs = require('node:fs');
+const options = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('cert.crt'),
+};
+
 const { Server } = require('socket.io');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(options,app);
 const io = new Server(server);
 
 const PORT = 3000;
